6b1dc6e7460c93dbe90f2e9f67f4e1b1be2d0b47
unknown <IB@DESKTOP-6IM55F2.(none))>
2026-02-09 15:18:10 +0100

    ma nvlle branche

--- SauceLabCucumber/src/test/java/configuration/DriverFactory.java
+++ SauceLabCucumber/src/test/java/configuration/DriverFactory.java
@@ -4,11 +4,16 @@ package configuration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
-
+import java.net.URL;
+//import java.net.m
 public class DriverFactory {
 
     public static WebDriver driver;
@@ -23,8 +28,18 @@ public class DriverFactory {
             prefs.put("profile.default_content_settings.popups", 0);
             options.addArguments("--incognito");
             options.setExperimentalOption("prefs", prefs);
-            driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+           // driver = new ChromeDriver(options);
+            //configuration selenium grid au lieu du chrome
+            EdgeOptions options1= new EdgeOptions();
+            options.addArguments("--remote-allow-origins=*");
+            ChromeOptions options2 = new ChromeOptions();
+
+        try {
+            driver = new RemoteWebDriver(new URL("http://172.16.14.110:4444/wd/hub"),options2);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
         }
         return driver;
     }
